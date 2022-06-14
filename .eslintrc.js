d6ea46570c100ac7a51eec5acd8a1d0bc1c3f417
Dicky Ardiar <42902627+Dickyrdiar@users.noreply.github.com>
2022-06-14 03:31:45 +0000

    adding router

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,20 @@
+/* eslint-disable no-undef */
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jets": true
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/react-in-jsx-scope": "off"
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+};
---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+export const plugins = {
+  tailwindcss: {},
+  autoprefixer: {}
+};
