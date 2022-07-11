1d283e104e64688752fd4100d4d3b5845584b909
dicky ardiar <diackyardiar1@gmail.com>
2022-07-11 15:36:15 +0700

    adding footer

--- src/router.js
+++ src/router.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Switch, Routes, Route } from 'react-router-dom';
 import App from '../src/App';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 const AppRouter = () => {
   return (
     <Switch>
-      <Navbar>
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </Navbar>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+      <Footer />
     </Switch>
   );
 };
