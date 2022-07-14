0bd66d230624799a3edb35cc74e4ab71ad257718
meimanjayagea <jayameiman17@gmail.com>
2022-07-14 23:08:27 +0700

    update commponent hello

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import logo from '../../logo.svg';
+import '../../App.css';
 
 const Dashboard = () => {
   return (
     <div className="bg-slate-400" style={{ height: 'calc(800px)' }}>
-      <p></p>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
   );
 };
